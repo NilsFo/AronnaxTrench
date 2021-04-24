32f7b312656d9029f945f7c823ec63cbb21cd107
NilsFo <nils.foerster@yahoo.de>
2021-04-24 13:13:12 +0200

    Added fish spawning & fish ai & fish DB

---
+++ Assets/Scripts/FishAI.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FishAI : MonoBehaviour, IPointerClickHandler
+{
+
+    public TextAsset fishDataCSV;
+    public float currentCountDown;
+    public int id=0;
+    public int minDepth;
+    public int maxDepth;
+    public string fishName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitData(id);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCountDown = currentCountDown + Time.deltaTime;
+        Transform t = GetComponent<Transform>();
+        t.position = new Vector3(t.position.x+currentCountDown, t.position.y,t.position.z);
+
+        if(currentCountDown > 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    public bool IsFishClickState()
+    {
+        // TODO: Check if camera is equipped here
+        return true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void InitData(int id)
+    {
+        string rawData = fishDataCSV.text;
+        string[] rows = rawData.Split('\n');
+
+        string row = rows[id+1];
+        string[] data = row.Split(';');
+
+        this.id = int.Parse(data[0]);
+        this.fishName = data[1];
+        this.minDepth = int.Parse(data[2]);
+        this.maxDepth = int.Parse(data[3]);
+    }
+}
---
+++ Assets/Scripts/FishManagerAI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishManagerAI : MonoBehaviour
+{
+
+    public GameObject fishPrefab;
+    public Camera playerCamera;
+    public Canvas submarineCanvas;
+    public int spawnDelay = 3;
+    public int spawnDelayJitter = 2;
+
+    public float currentCountDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitFishSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCountDown = currentCountDown - Time.deltaTime;
+ 
+        if(currentCountDown < 0)
+        {
+            SpawnNextFish();
+            InitFishSpawn();
+        }
+    }
+
+    public void SpawnNextFish()
+    {
+        int startY = Random.Range(50, 350);
+        GameObject fish = Instantiate(fishPrefab, new Vector3(0, startY, 0), Quaternion.identity);
+
+        fish.transform.SetParent(submarineCanvas.transform);
+        FishAI fai = fish.GetComponent<FishAI>();
+
+        int id = Random.Range(0, 4);
+        fai.id = id;
+    }
+
+    public void InitFishSpawn()
+    {
+        var jitter = Random.Range(spawnDelayJitter * -1, spawnDelayJitter);
+
+        currentCountDown = spawnDelay + jitter;
+        currentCountDown = Mathf.Max(currentCountDown, 1);
+    }
+
+    public int GetSubmarineDepth()
+    {
+        return 5;
+    }
+}
