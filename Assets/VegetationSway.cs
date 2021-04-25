ff0f2fbde8f6d1fef990adf9e804ed7644cd3b50
Patrick Herbers <patrick.herbers@rub.de>
2021-04-25 14:47:07 +0200

    Added swaying seaweed

---
+++ Assets/VegetationSway.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegetationSway : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float swayAmount = 5f;
+    public float swaySpeed = 1f;
+    private Quaternion initRot;
+    void Start()
+    {
+        initRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRot = Mathf.Sin(Time.time * swaySpeed) * swayAmount;
+        transform.rotation = initRot * Quaternion.Euler(xRot, 0, 0);
+    }
+}
