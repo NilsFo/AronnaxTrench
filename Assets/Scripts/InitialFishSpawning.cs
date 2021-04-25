d0eda6f23d762730bf275cc3511a8bfcc5fc79b2
NilsFo <nils.foerster@yahoo.de>
2021-04-25 19:21:39 +0200

    Added legendary fish AI & placements

--- Assets/Scripts/InitialFishSpawning.cs
+++ Assets/Scripts/InitialFishSpawning.cs
@@ -15,8 +15,6 @@ public class InitialFishSpawning : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-
         for(int i = 0; i < creationCount; i++)
         {
             float x = spawnOrigin.position.x;
